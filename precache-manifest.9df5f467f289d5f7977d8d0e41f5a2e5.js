8e7e7860ed4b9fcffb8a4932513f8c41a19d4746
Shull <nathansh@tmctrans.com>
2020-04-21 16:52:11 -0500

    Updates

---
+++ precache-manifest.9df5f467f289d5f7977d8d0e41f5a2e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae889a30a2787e896ebd437f9923480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3b4547afacdecdd785e",
+    "url": "/static/css/main.15d9aa86.chunk.css"
+  },
+  {
+    "revision": "43065990c2781ff5269a",
+    "url": "/static/js/2.47700140.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47700140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b4547afacdecdd785e",
+    "url": "/static/js/main.a5b0bf87.chunk.js"
+  },
+  {
+    "revision": "14a471dba854683c830a",
+    "url": "/static/js/runtime-main.61de1b92.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.PNG"
+  }
+]);
